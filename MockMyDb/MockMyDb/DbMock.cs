bb43dd7945644ef5f1037dfa8ff8fd3c4dd04336
Robin <robin.kooyman.work@gmail.com>
2020-02-22 19:51:20 +0100

    Fix creation

--- MockMyDb/MockMyDb/DbMock.cs
+++ MockMyDb/MockMyDb/DbMock.cs
@@ -1,26 +1,48 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Data.Common;
 using System.Data.SqlClient;
 
 namespace MockMyDb
 {
-    public class DbMock<T> : IDisposable where T : DbContext , new()
+    public sealed class DbMock<T> : IDisposable where T : DbContext , new()
     {
-        public T Context { get; }
-        public DbMock(DbContext context,Func<DbContextOptions<T>,T> createContext)
+        public T MockContext { get; }
+        public T OriginalContext { get; }
+        public DbMock(T context,Func<DbContextOptions<T>,T> createContext)
         {
-            var connection = context.Database.GetDbConnection();
+            var mockConnection = CreateDatabase(context);
             var optionsBuilder = new DbContextOptionsBuilder<T>();
-            connection.ChangeDatabase($"MockDatabase-{Guid.NewGuid()}");
-            optionsBuilder.UseSqlServer(connection);
-            Context = createContext(optionsBuilder.Options);
+            optionsBuilder.UseSqlServer(mockConnection);
+            MockContext = createContext(optionsBuilder.Options);
             //Create the database through a migration
-            CreateDatabase(context.Database.GenerateCreateScript());
+            CreateTables(context.Database.GenerateCreateScript());
         }
 
-        private void CreateDatabase(string createScript)
+        public string GenerateCreateScriptUsed()
         {
-            var connection = Context.Database.GetDbConnection();
+            return OriginalContext.Database.GenerateCreateScript();
+        }
+
+        private DbConnection CreateDatabase(DbContext context)
+        {
+            var databaseName = $"MockDatabase{context.Database.GetDbConnection().DataSource}-{Guid.NewGuid()}";
+            var connection = OriginalContext.Database.GetDbConnection();
+            connection.Open();
+            using (var command = connection.CreateCommand())
+            {
+#pragma warning disable CA2100 // Review SQL queries for security vulnerabilities
+                command.CommandText = $"CREATE DATABASE {databaseName}";
+#pragma warning restore CA2100 // Review SQL queries for security vulnerabilities
+                command.ExecuteNonQuery();
+            }
+            connection.ChangeDatabase(databaseName);
+            return connection;
+        }
+
+        private void CreateTables(string createScript)
+        {
+            var connection = MockContext.Database.GetDbConnection();
             connection.Open();
             using (var command = connection.CreateCommand())
             {
@@ -33,7 +55,7 @@ namespace MockMyDb
 
         public void Dispose()
         {
-            Context.Database.EnsureDeleted();
+            MockContext.Database.EnsureDeleted();
         }
     }
 }
