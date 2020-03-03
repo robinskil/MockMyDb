15179e374d63f24ffce685ecbf716090e360ebb9
Robin <robin.kooyman.work@gmail.com>
2020-03-03 15:39:52 +0100

    Removed test creator

--- MockMyDb/TestDatabaseCreator/Program.cs
+++ MockMyDb/TestDatabaseCreator/Program.cs
@@ -4,9 +4,12 @@ namespace TestDatabaseCreator
 {
     class Program
     {
+        public const string sqlServerConnectionString = @"Server=DESKTOP-MNMDILM\TASKAPP;Database=QueryAggregator;Integrated Security=true;";
+        public const string postgresConnectionString = @"Server=DESKTOP-MNMDILM\TASKAPP;Database=QueryAggregator;Integrated Security=true;";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
+}
 }
